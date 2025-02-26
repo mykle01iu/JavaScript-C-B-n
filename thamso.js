baa7b33e238785833f1b43c50dff1db105936391
Hoàng Đình Khải <142211107+mykle01iu@users.noreply.github.com>
2025-02-26 17:05:17 +0700

    Add files via upload

---
+++ thamso.js
@@ -0,0 +1,46 @@
+//Number type
+var a = 1;
+var b = 2;
+var c = 1.5;
+
+//String type
+var fullName = 'Dinh Khai';
+
+//Boolean type
+var isSuccess = true;
+
+//Undefined type
+var age;
+console.log(age);
+
+//Null
+var isNull = null; //Nothing
+
+console.log(isNull)
+
+//Symbol 
+var id = Symbol('id'); //unique: tính duy nhất
+var id2 = Symbol('id');
+
+console.log(id);
+
+//Function
+
+var myFunction = function() {
+    alert("Hi, xin chào các bạn!");
+}
+
+myFunction();
+
+//Object type
+
+var myObject = {
+    name: 'Dinh Khai',
+    age: 18,
+    adress: 'HaNoi',
+    myFunction: function (){
+
+    }
+};
+
+var myArray = [];
