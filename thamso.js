88b811be71dbe1d154bf39211f21ca11cb81d2ee
Hoàng Đình Khải <142211107+mykle01iu@users.noreply.github.com>
2025-02-26 17:08:47 +0700

    Add files via upload

---
+++ thamso.js
@@ -0,0 +1,13 @@
+function writeLog(message){ //(message): tham số hàm
+    console.log(message);
+}
+
+writeLog('Test Message'); //('Test Message'): đối số
+
+/*Nhiều tham số*/
+function yourName(name1, name2){
+    console.log(name1)
+    console.log(name2)
+}
+
+yourName('Khai','Hoang');
