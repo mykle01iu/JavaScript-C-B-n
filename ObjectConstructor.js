c65c2b8f44bced292b3d8cf5dec5fa2a47e9e1b0
Hoàng Đình Khải <142211107+mykle01iu@users.noreply.github.com>
2025-03-02 10:15:24 +0700

    Add files via upload

---
+++ ObjectConstructor.js
@@ -0,0 +1,20 @@
+
+//Object cinstructor
+
+function User(firstName, lastName, avarta) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.avarta = avarta;
+    this.getName = function(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+var author = new User('Hoang', 'Khai', 'Avatar');
+var user = new User('Doo','Nal','Avarta')
+
+author.tittle = 'Welcome to Hoang Dinh Khai'
+user.comment = 'Hay quá anh ơi'
+
+console.log(author.getName());
+console.log(user);
