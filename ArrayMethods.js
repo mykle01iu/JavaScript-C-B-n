4c3c37b16d9f6ac81e8de82c1f39d3d2bfb74f0b
Hoàng Đình Khải <142211107+mykle01iu@users.noreply.github.com>
2025-03-05 20:44:46 +0700

    arraymethods

---
+++ ArrayMethods.js
@@ -0,0 +1,65 @@
+
+var courses = [
+    {
+        id: 1,
+        name: 'JavaScript',
+        coin: 250
+    },
+    {
+        id: 2,
+        name: 'HTML, CSS',
+        coin: 0
+    },
+    {
+        id: 3,
+        name: 'Ruby',
+        coin: 0
+    },
+    {
+        id: 4,
+        name: 'PHP',
+        coin: 400
+    },
+    {
+        id: 5,
+        name: 'ReactJS',
+        coin: 500
+    },
+    {
+        id: 6,
+        name: 'Ruby',
+        coin: 330
+    },
+];
+
+//1.forEach()
+courses.forEach(function(course, index) {
+    console.log(index,course);
+
+}); 
+
+//2.every() >> Boolean: kiểm tra tất cả điều kiện đúng
+var isFree = courses.every(function(course, index){
+    console.log(index);
+    return course.coin == 0; 
+});
+console.log(isFree); //false
+
+//3.some() >> Boolean: kiểm tra 1 trong số điều kiện đúng
+var isTrue = courses.some(function(course, index){
+    console.log(index);
+    return course.coin == 400;
+});
+console.log(isTrue); //true
+
+//4.find(): trả về 1 phần tử thỏa mãn
+var timKiem = courses.find(function(course){
+    return course.name ===  'Ruby';
+});
+console.log(timKiem); //{id: 3, name: 'Ruby', coin: 0}
+
+//5.filter(): giống find nhưng trả về tất cả các phần tử thỏa mãn
+var timKiem2 = courses.filter(function(course){
+    return course.name ===  'Ruby';
+});
+console.log(timKiem2);
