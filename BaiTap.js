28c634fd2c496e2b6c4e0a946e7f3283ba0d1b26
Hoàng Đình Khải <142211107+mykle01iu@users.noreply.github.com>
2025-03-28 13:00:20 +0700

    Add files via upload

--- BaiTap.js
+++ BaiTap.js
@@ -1,109 +1,92 @@
-var watchList = [
-    {
-      "Title": "Inception",
-      "Year": "2010",
-      "Rated": "PG-13",
-      "Released": "16 Jul 2010",
-      "Runtime": "148 min",
-      "Genre": "Action, Adventure, Crime",
-      "Director": "Christopher Nolan",
-      "Writer": "Christopher Nolan",
-      "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
-      "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
-      "Language": "English, Japanese, French",
-      "Country": "USA, UK",
-      "imdbRating": "8.8",
-      "imdbVotes": "1,446,708",
-      "imdbID": "tt1375666",
-      "Type": "movie",
-    },
-    {
-      "Title": "Interstellar",
-      "Year": "2014",
-      "Rated": "PG-13",
-      "Released": "07 Nov 2014",
-      "Runtime": "169 min",
-      "Genre": "Adventure, Drama, Sci-Fi",
-      "Director": "Christopher Nolan",
-      "Writer": "Jonathan Nolan, Christopher Nolan",
-      "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
-      "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-      "Language": "English",
-      "Country": "USA, UK",
-      "imdbRating": "8.6",
-      "imdbVotes": "910,366",
-      "imdbID": "tt0816692",
-      "Type": "movie",
-    },
-    {
-      "Title": "The Dark Knight",
-      "Year": "2008",
-      "Rated": "PG-13",
-      "Released": "18 Jul 2008",
-      "Runtime": "152 min",
-      "Genre": "Action, Adventure, Crime",
-      "Director": "Christopher Nolan",
-      "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
-      "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
-      "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
-      "Language": "English, Mandarin",
-      "Country": "USA, UK",
-      "imdbRating": "9.0",
-      "imdbVotes": "1,652,832",
-      "imdbID": "tt0468569",
-      "Type": "movie",
-    },
-    {
-      "Title": "Batman Begins",
-      "Year": "2005",
-      "Rated": "PG-13",
-      "Released": "15 Jun 2005",
-      "Runtime": "140 min",
-      "Genre": "Action, Adventure",
-      "Director": "Christopher Nolan",
-      "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
-      "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
-      "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
-      "Language": "English, Urdu, Mandarin",
-      "Country": "USA, UK",
-      "imdbRating": "8.3",
-      "imdbVotes": "972,584",
-      "imdbID": "tt0372784",
-      "Type": "movie",
-    },
-    {
-      "Title": "Avatar",
-      "Year": "2009",
-      "Rated": "PG-13",
-      "Released": "18 Dec 2009",
-      "Runtime": "162 min",
-      "Genre": "Action, Adventure, Fantasy",
-      "Director": "James Cameron",
-      "Writer": "James Cameron",
-      "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
-      "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
-      "Language": "English, Spanish",
-      "Country": "USA, UK",
-      "imdbRating": "7.9",
-      "imdbVotes": "876,575",
-      "imdbID": "tt0499549",
-      "Type": "movie",
-    }
-  ];
+//Bai1:
 
-function calculateRating(watchList){
-    var isDirector = watchList.filter(function(movie){
-        return movie.Director == 'Christopher Nolan';
-    });
+const students = [
+    { id: 1, name: "An", score: 85 },
+    { id: 2, name: "Bình", score: 92 },
+    { id: 3, name: "Cường", score: 75 },
+    { id: 4, name: "Duy", score: 95 },
+    { id: 5, name: "Lan", score: 89 }
+   ];
 
-    var totalRate = isDirector.reduce(function(total, movie){
-       return total += parseFloat(movie.imdbRating);
-    }, 0)
-    return totalRate / isDirector.length;
+var TopStudent = (students) => {
+    const maxScore = students.reduce((max, student)  =>
+        student.score > max ? student.score: max, students[0].score)
+
+    return students.find(student => student.score === maxScore);
 }
 
-  
-  // Expected results
-  console.log(calculateRating(watchList)); // Output: 8.675
-  
-  
+console.log(TopStudent(students))
+
+
+//Bai4:
+const products = [
+    { id: 1, name: "iPhone", price: 1000 },
+    { id: 2, name: "Samsung", price: 900 },
+    { id: 3, name: "Xiaomi", price: 600 },
+    { id: 4, name: "Oppo", price: 700 }
+   ];
+var NamePhones = (products) => {
+    return products.map(phone => phone.name.toUpperCase())
+}   
+
+console.log(NamePhones(products))
+
+
+//Bai3:
+const employees = [
+    { name: "A", position: "Developer", salary: 2000 },
+   { name: "B", position: "Designer", salary: 1800 },
+    { name: "C", position: "Manager", salary: 5000 },
+    { name: "D", position: "Developer", salary: 2500 },
+    { name: "E", position: "Developer", salary: 3000 }
+   ];
+
+var Congty = (employees) => {
+    return employees.filter(emp => emp.position === 'Developer' && emp.salary >= 2000)
+};
+
+console.log(Congty(employees));
+
+//Bai2
+const inventory = [
+    { name: "Laptop", stock: 10 },
+    { name: "Mouse", stock: 2 },
+    { name: "Keyboard", stock: 5 },
+    { name: "Monitor", stock: 0 },
+    { name: "USB", stock: 1 }
+   ];
+
+var HangTon = (inventory) => {
+    return inventory.some(i => i.stock <= 2)
+}
+
+console.log(HangTon(inventory))
+
+//Bai5
+const cart = [
+    { name: "apple", price: 2, quantity: 4 },
+    { name: "banana", price: 1, quantity: 6 },
+    { name: "orange", price: 3, quantity: 3 }
+   ];
+
+var Sum = (cart) =>  {
+    return cart.reduce((acc, n) => acc + (n.price) * (n.quantity), 0);
+};
+console.log(Sum(cart))
+
+//Bai6
+const orders = {
+    order1: { customer: "Nguyen Van A", items: [{ name: "Laptop", price: 1000,
+   quantity: 1 }, { name: "Mouse", price: 50, quantity: 2 }] },
+    order2: { customer: "Tran Thi B", items: [{ name: "Keyboard", price: 80,
+   quantity: 1 }, { name: "Monitor", price: 300, quantity: 2 }] },
+    order3: { customer: "Le Van C", items: [{ name: "USB", price: 20, quantity:
+   5 }, { name: "Headset", price: 150, quantity: 1 }] }
+   };
+//1.Lay danh sach ma don hang
+var MaDonHang = Object.keys(orders)
+console.log(MaDonHang)
+
+var TongGiaTriDonHang = (orders) => {
+    return orders.filter(sum => sum.MaDonHang.price)
+}
